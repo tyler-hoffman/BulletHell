61a0fe5145fc54c5ae1dc6a8789a929180d1f02d
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2015-10-13 09:42:29 -0400

    Add rectangle.rotate

--- public/d2/utils/rectangle.js
+++ public/d2/utils/rectangle.js
@@ -2,6 +2,8 @@
 
 define(function() {
 
+  var rotate
+
   var Rectangle = function(x, y, width, height) {
     this.float32Array = new Float32Array(12);
     this.set(x, y, width, height);
@@ -26,25 +28,66 @@ define(function() {
   };
 
   Rectangle.prototype.set = function(x, y, width, height) {
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
+    if (typeof x === 'number') {
 
-    var x1 = x,
-        x2 = x + width,
-        y1 = y,
-        y2 = y + height;
+      // correct so width and height are non-negative
+      if (width < 0) {
+        x -= width;
+        width = -width
+      }
 
-    var rect = this.float32Array;
-    rect[0] = x1; rect[1] = y1;
-    rect[2] = x2; rect[3] = y1;
-    rect[4] = x1; rect[5] = y2;
-    rect[6] = x1; rect[7] = y2;
-    rect[8] = x2; rect[9] = y1;
-    rect[10] = x2; rect[11] = y2;
+      if (height < 0) {
+        y -= height;
+        height = -height;
+      }
+
+      this.x = x;
+      this.y = y;
+      this.width = width;
+      this.height = height;
+
+      var x1 = x,
+          x2 = x + width,
+          y1 = y,
+          y2 = y + height;
+
+      var rect = this.float32Array;
+      rect[0] = x1; rect[1] = y1;
+      rect[2] = x2; rect[3] = y1;
+      rect[4] = x1; rect[5] = y2;
+      rect[6] = x1; rect[7] = y2;
+      rect[8] = x2; rect[9] = y1;
+      rect[10] = x2; rect[11] = y2;
+
+    } else if (x === undefined) {
+
+      // not all parameters passed
+      return this.set(x || 0, y || 0, width || 0, height || 0);
+
+    } else {
+
+      // x is rectangle
+      return this.set(x.x, x.y, x.width, x.height);
+
+    }
+
+    return this;
   };
 
+  Rectangle.prototype.rotate = function(theta, center) {
+    var cosTheta = Math.cos(theta),
+        sinTheta = Math.sin(theta),
+        xLeft = this.x - center.x,
+        yTop = this.y - center.y;
+
+    this.set(
+      center.x + cosTheta * xLeft + sinTheta * yTop,
+      center.y + sinTheta * xLeft + cosTheta * yTop,
+      cosTheta * this.width + sinTheta * this.height,
+      sinTheta * this.width + cosTheta * this.height
+    );
+    return this;
+  };
 
   return Rectangle;
 
