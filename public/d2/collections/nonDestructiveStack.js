24b82f35f96189b1efc9ce6f4284a70fc1a1a43c
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2015-10-17 11:22:44 -0400

    Add nondestructiveStack

---
+++ public/d2/collections/nonDestructiveStack.js
@@ -0,0 +1,40 @@
+"use strict"
+
+define(function() {
+
+  var Stack = function(factory, capacity) {
+    this.currentIndex = 0;
+    this.elements = [];
+    this.factory = factory;
+
+    capacity = capacity || 0;
+    for (var i = 0; i < capacity; i++) {
+      this.push();
+    }
+  };
+
+  Stack.prototype.push = function() {
+    if (this.currentIndex === this.elements.length) {
+      this.elements.push(this.factory());
+    }
+    return this.elements[this.currentIndex++];
+  };
+
+  Stack.prototype.pop = function() {
+    return this.elements[--this.currentIndex];
+  };
+
+  Stack.prototype.peek = function() {
+    return this.elements[this.currentIndex - 1];
+  };
+
+  Stack.prototype.isEmpty = function() {
+    return this.currentIndex === 0;
+  };
+
+  Stack.prototype.size = function() {
+    return this.currentIndex;
+  };
+
+  return Stack;
+});
