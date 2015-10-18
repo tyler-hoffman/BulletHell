90c304c4a4377db96a49f16dd158b937e43c4219
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2015-10-18 04:32:38 -0400

    Add SoftSwinger emitterDecorator

---
+++ public/scripts/emitters/softSwinger.js
@@ -0,0 +1,28 @@
+"use strict"
+
+define([
+    'emitters/EmitterDecorator'
+  ], function(EmitterDecorator) {
+
+    var SoftSwinger = function(radiansPerSecond, spread) {
+      EmitterDecorator.call(this);
+
+      this.radiansPerSecond = radiansPerSecond;
+      this.spread = spread;
+      this.angle = 0;
+      this.time = 0;
+    };
+
+    SoftSwinger.prototype = new EmitterDecorator();
+
+    SoftSwinger.prototype.update = function(deltaTime) {
+      this.time += deltaTime * this.radiansPerSecond / this.spread;
+      this.angle = Math.sin(this.time);
+    };
+
+    SoftSwinger.prototype.decorateEmission = function(emission) {
+      emission.angle += this.angle;
+    };
+
+    return SoftSwinger;
+});
