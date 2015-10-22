758b6a762d45040e64f0e3ad74283373c8777f5c
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2015-10-21 21:55:46 -0400

    Add events

---
+++ public/d2/actors/event.js
@@ -0,0 +1,10 @@
+"use strict"
+
+define(function() {
+
+  var Event = function(name) {
+    this.name = name;
+  };
+
+  return Event;
+});
--- public/scripts/emitters/emitEvent.js
+++ public/scripts/emitters/emitEvent.js
@@ -1,11 +1,17 @@
 "use strict";
 
-define(function() {
+define(['d2/actors/event'], function(Event) {
+
+  const NAME = 'emit';
 
   var EmitEvent = function(emitted, emitter) {
+    Event.call(this, NAME);
+
     this.emitted = emitted;
     this.emitter = emitter;
   };
+
+  EmitEvent.prototype = new Event();
 
   return EmitEvent;
 });
