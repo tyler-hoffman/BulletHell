af5a4a9f8ea3752c1e33e3b3bf3cd9786bdd06d0
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2015-10-30 17:18:15 -0400

    Add SlideShowScene shell

---
+++ public/d2/scenes/slideShowScene.js
@@ -0,0 +1,18 @@
+"use strict"
+
+define(['d2/scenes/scene'], function(Scene) {
+
+  var SlideShowScene = function() {
+    Scene.call(this);
+  };
+
+  SlideShowScene.prototype.addSlide = function(image, duration) {
+
+  };
+
+  SlideShowScene.prototype.nextSlide = function() {
+
+  };
+
+  return SlideShowScene;
+});
