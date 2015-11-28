42d8947a4bb8127f748a99c7ca61676e1edd6b2e
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2015-11-28 12:41:58 -0500

    Fix fps reporting

--- public/scripts/utils/renderInfo.js
+++ public/scripts/utils/renderInfo.js
@@ -2,33 +2,18 @@
 
 define(function() {
 
-  var average = function(numbers) {
-    var sum = 0;
-    for (var i in numbers) {
-      sum += numbers[i];
-    }
-
-    /*
-     * divide by length + 1 because when time >= reportFrequency
-     * 2 numbers are added to the history
-     */
-    return sum / (numbers.length - 1);
-  };
-
   /**
    * Create a new RenderInfo.
    * @param {Number} reportFrequency seconds between reports
    * @param {Function} callback function to do the reporting
    */
   var RenderInfo = function(reportFrequency, callback) {
-    this.totalTime = 0;
     this.reportFrequency = reportFrequency || 1;
     this.callback = callback;
 
+    this.time = 0;
+    this.frames = 0;
     this.fps = 0;
-    this.history = [];
-
-    this.reset();
   };
 
   /**
@@ -37,53 +22,25 @@ define(function() {
    * @param {Number} deltaTime Seconds for most recent frame
    */
   RenderInfo.prototype.update = function(deltaTime) {
-    this.totalTime += deltaTime;
-    if (deltaTime) {
+    this.time += deltaTime;
+    this.frames++;
 
-      var totalTime = this.time + deltaTime;
+    if (this.time >= this.reportFrequency) {
 
-      if (totalTime >= this.reportFrequency) {
+      this.fps = this.frames / this.reportFrequency;
+      this.callback && this.callback.call(this.context, this.fps);
 
-        var untilReport = this.reportFrequency - this.time,
-            remainder = deltaTime - untilReport;
-
-        // finish current
-        this.addTime(untilReport);
-        this.fps = Math.floor(average(this.history));
-        this.callback.call(this.context, this.fps);
-
-        // start anew
-        this.reset();
-        this.addTime(totalTime - this.reportFrequency);
-
-      } else {
-
-        this.addTime(deltaTime);
-
-      }
+      this.reset();
     }
+
   };
 
   /**
    * Reset the info.
    */
   RenderInfo.prototype.reset = function() {
-    this.itemsInHistory = 0;
-    this.time =  0;
-  };
-
-  RenderInfo.prototype.hardReset = function() {
-    this.reset();
-    this.totalTime = 0;
-  };
-
-  /**
-   * Add time.
-   * @param {Number} time Time to add
-   */
-  RenderInfo.prototype.addTime = function(time) {
-    this.history[this.itemsInHistory++] = 1 / time;
-    this.time += time;
+    this.time = this.time % this.reportFrequency;
+    this.frames = 0;
   };
 
   /**
